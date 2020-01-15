b1811d2e8f9833c7f9acef01107df1b1a36aae22
tguyer <tguyer@gmail.com>
2020-01-15 01:49:00 -0500

    added logic for metadata in the tournmanet controller, but it's still not fully complete

--- Tournament/TournamentController.js
+++ Tournament/TournamentController.js
@@ -70,19 +70,48 @@ const tournamentController = function () {
         // Grab the callback from the request
         const { callback } = req.body;
 
-        // Grab fields from the callback that we will need to parse match results
+        // Grab fields from the match callback that relate to the actual match. Mainly, these are needed for
+        // making a call to Riot's API to get the actual match results.
         let gameId = callback.get('gameId');
         let tournamentCode = callback.get('shortCode');
+        let apiReportedMatchResults = {};  // TODO: Use a call to Riot's API to get the actual match results
+
+        // The match metadata is some json that we can tie to a tournament match. Presumably we will send things like
+        // team names, player names, the event, etc. Right now I imagine it will look something like this, but by the
+        // time you're reading this comment it's probably changed:
+        //  {
+        //      'risenData': {
+        //          Dates and stuff. When the code was generated, by whom, etc
+        //      },
+        //      'event': {
+        //          idk man, I haven't figured out what fields we need for events
+        //      },
+        //      'team1': {
+        //          'teamName': '',
+        //          'members': []
+        //      }.
+        //      'team2': {
+        //          'teamName': '',
+        //          'members': []
+        //      }
+        // }
         let metaData = callback.get('metaData');
-        let team1Name = metaData.get('team1');
-        let team2Name = metaData.get('team2');
+
+        // The actual participants and the expected participants are very likely to differ. As unfortunate as it is,
+        // it's the truth. The silver lining is that the difference between them to implies some interesting
+        // information (if not in the expected list, but in the actual list, that person is probably a substitute).
+        // That's good information; REALLY good. We can definitely use it to improve the API.
         let winningTeamMembers = callback.get('winningTeam');
         let losingTeamMembers = callback.get('losingTeam');
-        let allPlayers = winningTeamMembers.append(losingTeamMembers);
+        let allActualMatchParticipants = winningTeamMembers.append(losingTeamMembers);
+        // I know this isn't the cleanest way but I'm feeling SPICY
+        let allExpectedMatchParticipants = metaData.get('team1').get('members').append(
+            metaData.get('team2').get('members')
+        );
 
-        // Given the summoner names in the match callback, look them up in the risen database. If they don't have a Risen
+        // Given the summoner names in the metadata, look them up in the risen database. If they don't have a Risen
         // Player Id, create a new Risen Player for them and assign them one.
-        allPlayers.forEach((summonerObj) => {
+        allActualMatchParticipants.forEach((summonerObj) => {
             let summonerName = summonerObj.get('summonerName');
             let playerReq = {
                 body: {
@@ -93,8 +122,13 @@ const tournamentController = function () {
             if (!risenId) {
                 // If there was not a risen id, we need to create a new risen player for this summoner
                 // TODO
+            } else {
+                /*
+                 * For some reason, I hate else blocks. Code just looks so much cleaner if the else isn't there. You
+                 * know it, I know it, we all know it. But I get the itch that it might be useful for *something* later,
+                 * so I'm putting it here in hopes that I see it one day and go "oh man, I can do <something> there!"
+                 */
             }
-            
         });
     };
 
